41bbf8cf6c61bdde1523cf8a720e0a9487a0a6d2
Fran Real <fran.real.perez@gmail.com>
2018-03-04 23:34:16 +0100

    Add config file for joy layout

--- ual_teleop/scripts/joy_teleop.py
+++ ual_teleop/scripts/joy_teleop.py
@@ -1,90 +1,12 @@
 #!/usr/bin/env python
 import yaml
 import rospy
+import rospkg
 from sensor_msgs.msg import Joy
 from enum import Enum
 from uav_abstraction_layer.srv import *
 from geometry_msgs.msg import TwistStamped
 from std_msgs.msg import String
-
-config = """
-joy_layout:
-  a:
-    type: button
-    index: 0
-  b:
-    type: button
-    index: 1
-  x:
-    type: button
-    index: 3
-  y:
-    type: button
-    index: 4
-
-  left_shoulder:
-    type: button
-    index: 6
-
-  right_shoulder:
-    type: button
-    index: 7
-
-  left_trigger:
-    type: button
-    index: 8
-
-  right_trigger:
-    type: button
-    index: 9
-
-  select:
-    type: button
-    index: 10
-
-  start:
-    type: button
-    index: 11
-
-  left_thumb:
-    type: button
-    index: 13
-
-  right_thumb:
-    type: button
-    index: 14
-
-  left_analog_x:
-    type: axis
-    index: 0
-    reversed: true
-
-  left_analog_y:
-    type: axis
-    index: 1
-    reversed: false
-
-  right_analog_x:
-    type: axis
-    index: 2
-    reversed: true
-
-  right_analog_y:
-    type: axis
-    index: 3
-    reversed: false
-
-  dpad_x:
-    type: axis
-    index: 6
-    reversed: true
-
-  dpad_y:
-    type: axis
-    index: 7
-    reversed: false
-
-"""
 
 ButtonState = Enum('ButtonState', 'UNKNOWN JUST_PRESSED PRESSED JUST_RELEASED RELEASED')
 
@@ -119,10 +41,14 @@ def update_button_state(prev_state, value):
 
 class JoyHandle:
 
-    def __init__(self):
+    def __init__(self, config_file):
         self.ros_data = Joy()
         self.buttons_state = []
-        self.layout = yaml.load(config)['joy_layout']
+        with open(config_file, 'r') as config:
+            try:
+                self.layout = yaml.load(config)['joy_layout']
+            except yaml.YAMLError as error:
+                print(error)
 
     def update(self, data):
         self.ros_data = data
@@ -149,9 +75,8 @@ class JoyHandle:
 
 class JoyTeleop:
 
-    def __init__(self):
-        self.joy_handle = JoyHandle()
-        robot_id = 'uav_1'  # TODO(franreal): other id?
+    def __init__(self, joy_file, robot_id):
+        self.joy_handle = JoyHandle(joy_file)
         take_off_url = robot_id + '/ual/take_off'
         land_url =     robot_id + '/ual/land'
         velocity_url = robot_id + '/ual/set_velocity'
@@ -184,10 +109,12 @@ class JoyTeleop:
             self.velocity(vel_cmd)
 
 def main():
-    rospy.init_node("joy_teleop", anonymous=True)
-    teleop = JoyTeleop()
-    rospy.Subscriber("uav_1/ual/state", String, teleop.state_callback)
-    rospy.Subscriber("joy", Joy, teleop.joy_callback)
+    rospy.init_node('joy_teleop', anonymous=True)
+    joy_file = rospkg.RosPack().get_path('ual_teleop') + '/config/8bitdo_nes30_pro.yaml'  # TODO(franreal): argument!
+    robot_id = 'uav_1'  # TODO(franreal): argument!
+    teleop = JoyTeleop(joy_file, robot_id)
+    rospy.Subscriber(robot_id + '/ual/state', String, teleop.state_callback)
+    rospy.Subscriber('joy', Joy, teleop.joy_callback)
     rospy.spin()
 
 if __name__ == '__main__':
