fad0ea61d9a05a801b0ea0a56572030c65cacf33
Alejandro <ale.acc@hotmail.com>
2019-03-11 15:26:37 +0100

    add _JA.py files

---
+++ px4_bringup/scripts/launch_gzworld_JA.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python
+import subprocess
+import argparse
+import os
+import time
+import signal
+import utils
+import rospkg
+import rospy
+
+
+def main():
+
+    # Parse arguments
+    parser = argparse.ArgumentParser(description="Launch gazebo world simulation, \
+    similar to roslaunch gazebo_ros empty_world.launch with some needed extras for px4 sitl")
+    parser.add_argument('-physics', type=str, default='ode', help="Gazebo physics engine to use")
+    parser.add_argument('-world', type=str, default='worlds/empty.world',
+                        help="World file name with respect to GAZEBO_RESOURCE_PATH")
+    parser.add_argument('-add_model_path', type=str, default='',
+                        help="Path to add to GAZEBO_MODEL_PATH")
+    parser.add_argument('-description_package', type=str, default="robots_description",
+                        help='robot description package, must follow robots_description file structure')
+    args, unknown = parser.parse_known_args()
+    utils.check_unknown_args(unknown)
+
+    ### JA CODE ###
+    gui = rospy.get_param('gazebo_gui')
+    ### JA CODE END ###
+
+    # Get an instance of RosPack with the default search paths
+    rospack = rospkg.RosPack()
+    px4_dir = rospack.get_path('px4')
+
+    # Set environment variables as in px4/Firmware/Tools/setup_gazebo.bash
+    gz_env = os.environ.copy()
+    current_gz_plugin_path = gz_env.get('GAZEBO_PLUGIN_PATH', '')
+    gz_env['GAZEBO_PLUGIN_PATH'] = px4_dir + '/build/posix_sitl_default/build_gazebo' + \
+                                   ':' + current_gz_plugin_path
+    current_gz_model_path = gz_env.get('GAZEBO_MODEL_PATH', '')
+    # Always include robots_description parent path
+    robots_description_parent_path = os.path.abspath(os.path.join(\
+                                              rospack.get_path('robots_description'), os.pardir))
+    gz_env['GAZEBO_MODEL_PATH'] = px4_dir + '/Tools/sitl_gazebo/models' + \
+                                 ':' + robots_description_parent_path + \
+                                 ':' + current_gz_model_path
+    # Include description_package parent path if it is not robots_description
+    if args.description_package is not "robots_description":
+        description_package_parent_path = os.path.abspath(os.path.join(\
+                                              rospack.get_path(args.description_package), os.pardir))
+        gz_env['GAZEBO_MODEL_PATH'] += ':' + description_package_parent_path
+
+    # TODO: add_model_path should be a list of paths? Or just a string separated by ':'?
+    if args.add_model_path:
+        gz_env['GAZEBO_MODEL_PATH'] += ':' + args.add_model_path
+
+    # print "gz_env['GAZEBO_MODEL_PATH'] = [%s]" % gz_env['GAZEBO_MODEL_PATH']  # debug
+
+    # Get map origin lat-lon-alt from rosparam
+    rospy.init_node('gazebo_world')
+    if rospy.has_param('~sim_origin'):
+        latlonalt = rospy.get_param('~sim_origin')
+    else:
+        latlonalt = [0.0, 0.0, 0.0]
+
+    # Set gazebo origin for px4 sitl mavlink interface plugin
+    gz_env['PX4_HOME_LAT'] = str(latlonalt[0])
+    gz_env['PX4_HOME_LON'] = str(latlonalt[1])
+    gz_env['PX4_HOME_ALT'] = str(latlonalt[2])
+
+    # Set use_sim_time flag to true
+    subprocess.call("rosparam set /use_sim_time true", shell=True)
+
+    # Create temporary directory for robot sitl stuff (id=None)
+    temp_dir = utils.temp_dir(None)
+    subprocess.call("mkdir -p " + temp_dir, shell=True)
+
+    # Start gazebo server
+    server_args = "rosrun gazebo_ros gzserver -e " + args.physics + ' ' + args.world + ' __name:=gazebo'
+    server_out = open(temp_dir + '/gzserver.out', 'w')
+    server_err = open(temp_dir + '/gzserver.err', 'w')
+    server = subprocess.Popen(server_args, stdout=server_out, stderr=server_err, cwd=temp_dir, \
+                                           env=gz_env, shell=True, preexec_fn=os.setsid)
+
+    # Start gazebo client. JA changed
+    if gui == True:
+        time.sleep(0.2)
+        client_args = "rosrun gazebo_ros gzclient __name:=gzclient"
+        client_out = open(temp_dir + '/gzclient.out', 'w')
+        client_err = open(temp_dir + '/gzclient.err', 'w')
+        client = subprocess.Popen(client_args, stdout=client_out, stderr=client_err, cwd=temp_dir, \
+                                            env=gz_env, shell=True, preexec_fn=os.setsid)
+
+    rospy.spin()  # Now I'm a ros node, jus wait
+
+    # Kill'em all. JA changed
+    if gui == True:
+        if client.poll() is None:
+            os.killpg(os.getpgid(client.pid), signal.SIGTERM)  # TODO: SIGKILL?
+    if server.poll() is None:
+        os.killpg(os.getpgid(server.pid), signal.SIGTERM)  # TODO: SIGKILL?
+    # Close log files. JA changed
+    if gui == True:
+        client_out.close()
+        client_err.close()
+    server_out.close()
+    server_err.close()
+
+
+if __name__ == '__main__':
+    main()
---
+++ px4_bringup/scripts/spawn_gzmodel_JA.py
@@ -0,0 +1,159 @@
+#!/usr/bin/env python
+import subprocess
+import argparse
+import utils
+import numpy
+import os
+import rospkg
+import rospy
+import time
+import xml.etree.ElementTree as ET
+
+
+def main():
+
+    # Parse arguments
+    parser = argparse.ArgumentParser(description='Spawn robot in Gazebo for SITL')
+    parser.add_argument('-model', type=str, default="mbzirc",
+                        help='robot model name, must match xacro description folder name')
+    parser.add_argument('-id', type=int, default=1,
+                        help='robot id, used to compute sim_port')
+    parser.add_argument('-x', type=float, default=0.0,
+                        help='initial x position')
+    parser.add_argument('-y', type=float, default=0.0,
+                        help='initial y position')
+    parser.add_argument('-z', type=float, default=0.0,
+                        help='initial z position')
+    parser.add_argument('-Y', type=float, default=0.0,
+                        help='initial yaw angle')
+    parser.add_argument('-description_package', type=str, default="robots_description",
+                        help='robot description package, must follow robots_description file structure')
+    parser.add_argument('-material', type=str, default="DarkGrey",
+                        help='robot Gazebo/material; \
+                        see materials/scripts/gazebo.material (at your gazebo version)')
+    parser.add_argument('-backend', type=str, default="mavros",
+                        help='backend to use')
+    args, unknown = parser.parse_known_args()
+    utils.check_unknown_args(unknown)
+
+    # Get an instance of RosPack with the default search paths
+    rospack = rospkg.RosPack()
+
+    # Init ros node
+    rospy.init_node('spawn_gzmodel_{}'.format(args.id))
+
+    #### JA change
+    uav_frame = rospy.get_param( '/uav_{}_home'.format(args.id) )
+    args.x = uav_frame['translation'][0]
+    args.y = uav_frame['translation'][1]
+    args.z = uav_frame['translation'][2]
+    args.Y = uav_frame['gz_initial_yaw']
+    args.model = uav_frame["model"]
+
+    # Xacro description must be in specified package
+    description_dir = rospack.get_path(args.description_package)
+
+    # Create temporary directory for robot sitl stuff
+    temp_dir = utils.temp_dir(args.id)
+    subprocess.call("mkdir -p " + temp_dir, shell=True)
+
+    # Get udp configuration, depending on id
+    udp_config = utils.udp_config(args.id)
+
+    # Check file type (xacro or sdf)
+    model_file_type = ""
+    for listed_file in sorted(os.listdir(description_dir + "/models/" + args.model)):
+        if listed_file == "model.sdf":
+            model_file_type = "sdf"
+            break
+        if listed_file == "model.xacro":
+            model_file_type = "xacro"
+            break
+
+    if model_file_type == "xacro":
+        xacro_description = description_dir + "/models/" + args.model + "/model.xacro"
+
+        # Create urdf from xacro description
+        temp_urdf = temp_dir + "/" + args.model + ".urdf"
+        xacro_args = "xacro --inorder -o " + temp_urdf + " " + \
+        xacro_description + \
+        " enable_mavlink_interface:=true" + \
+        " enable_gps_plugin:=true" + \
+        " enable_ground_truth:=false" + \
+        " enable_logging:=false" + \
+        " enable_camera:=false" + \
+        " enable_wind:=false" + \
+        " mavlink_udp_port:=" + str(udp_config["sim_port"]) + \
+        " visual_material:=" + args.material
+        xacro_out = open(temp_dir+"/xacro.out", 'w')
+        xacro_err = open(temp_dir+"/xacro.err", 'w')
+        subprocess.call(xacro_args, shell=True, stdout=xacro_out, stderr=xacro_err)
+        xacro_out.close()
+        xacro_err.close()
+
+        # Create sdf from urdf
+        temp_sdf = temp_dir + "/" + args.model + ".sdf"
+        subprocess.call("gz sdf -p " + temp_urdf + " > " + temp_sdf, shell=True)
+
+    elif model_file_type == "sdf":
+        model_sdf = description_dir + "/models/" + args.model + "/model.sdf"
+        temp_sdf = temp_dir + "/" + args.model + ".sdf"
+        subprocess.call("cp " + model_sdf + " " + temp_sdf, shell=True)
+
+        # Change simulation port
+        tree = ET.parse(temp_sdf)
+        root = tree.getroot()
+        model = root.find('model')
+        for plugintag in model.findall('plugin'):
+            if plugintag.get('name') == 'mavlink_interface':
+                porttag = plugintag.find('mavlink_udp_port')
+                porttag.text = str(udp_config["sim_port"])
+
+        # Typhoon_h480 patch - TODO use xacro instead
+        if args.model == 'typhoon_h480':
+            for plugintag in model.findall('plugin'):
+                if plugintag.get('name') == 'gimbal_controller':
+                    imutag = plugintag.find('gimbal_imu')
+                    imutag.text = 'typhoon_h480_' + str(args.id) + '::camera_imu'
+            for linktag in model.findall('link'):
+                if linktag.get('name') == 'cgo3_camera_link':
+                    for sensortag in linktag.findall('sensor'):
+                        if sensortag.get('name') == 'camera_imu':
+                            sensortag.set('name', 'typhoon_h480_' + str(args.id) + '::camera_imu')
+
+        tree.write(temp_sdf)
+
+    else:
+        raise IOError("Couldn't find model.sdf/model.xacro description file")
+
+    # Set gravity=0 for light simulations
+    if args.backend == 'light':
+        tree = ET.parse(temp_sdf)
+        root = tree.getroot()
+        model = root.find('model')
+        for linktag in model.findall('link'):
+            #if linktag.get('name') == 'base_link':
+            gravitytag = linktag.find('gravity')
+            if gravitytag == None:
+                gravitytag = ET.SubElement(linktag,'gravity')
+            gravitytag.text = '0'
+        tree.write(temp_sdf)
+
+    # Sleep for waiting the world to load
+    rospy.wait_for_service('/gazebo/spawn_sdf_model')
+    time.sleep(0.4)
+
+    # Minimum z to avoid collision with ground
+    z_min = 0.3
+
+    # Spawn robot sdf in gazebo
+    gzmodel_args = "gz model -f " + temp_sdf + \
+    " -m " + args.model + "_" + str(args.id) + \
+    " -x " + str(args.x) + \
+    " -y " + str(args.y) + \
+    " -z " + str(args.z+z_min) + \
+    " -Y " + str(args.Y)
+    subprocess.call(gzmodel_args, shell=True)
+
+if __name__ == "__main__":
+    main()
