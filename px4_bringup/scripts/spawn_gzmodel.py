9783c3d0e5ec187562103cf3046449bd12a29c16
Fran Real <fran.real.perez@gmail.com>
2019-07-31 17:50:34 +0200

    Add the option of specifying frame_id at spawn

--- px4_bringup/scripts/spawn_gzmodel.py
+++ px4_bringup/scripts/spawn_gzmodel.py
@@ -6,6 +6,8 @@ import numpy
 import os
 import rospkg
 import rospy
+import tf2_ros
+import math
 import time
 import xml.etree.ElementTree as ET
 
@@ -33,6 +35,8 @@ def main():
                         see materials/scripts/gazebo.material (at your gazebo version)')
     parser.add_argument('-backend', type=str, default="mavros",
                         help='backend to use')
+    parser.add_argument('-frame_id', type=str, default="map",
+                        help='initial position and yaw frame reference; id [map] refers to gazebo origin')
     args, unknown = parser.parse_known_args()
     utils.check_unknown_args(unknown)
 
@@ -139,13 +143,39 @@ def main():
     # Minimum z to avoid collision with ground
     z_min = 0.3
 
+    spawn_x = args.x
+    spawn_y = args.y
+    spawn_z = args.z
+    spawn_yaw = args.Y
+
+    if args.frame_id != 'map':
+        tf_buffer = tf2_ros.Buffer()
+        tf_listener = tf2_ros.TransformListener(tf_buffer)
+        try:
+            transform_stamped = tf_buffer.lookup_transform('map', args.frame_id, rospy.Time(0), rospy.Duration(10.0))
+            if transform_stamped.transform.rotation.x != 0:
+                raise ValueError('Only yaw rotations allowed at spawn; rotation.x should be 0, found {}'.format(transform_stamped.transform.rotation.x))
+            if transform_stamped.transform.rotation.y != 0:
+                raise ValueError('Only yaw rotations allowed at spawn; rotation.y should be 0, found {}'.format(transform_stamped.transform.rotation.y))
+            transform_yaw = 2.0 * math.atan2(transform_stamped.transform.rotation.z, transform_stamped.transform.rotation.w)
+            new_x = transform_stamped.transform.translation.x + spawn_x * math.cos(transform_yaw) - spawn_y * math.sin(transform_yaw)
+            new_y = transform_stamped.transform.translation.y + spawn_x * math.sin(transform_yaw) + spawn_y * math.cos(transform_yaw)
+            new_z = transform_stamped.transform.translation.z + spawn_z
+            new_yaw = transform_yaw + spawn_yaw
+            spawn_x = new_x
+            spawn_y = new_y
+            spawn_z = new_z
+            spawn_yaw = new_yaw
+        except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
+            rospy.logerr('Failed to lookup transform from [{}] to [map], ignoring frame_id'.format(args.frame_id))
+
     # Spawn robot sdf in gazebo
     gzmodel_args = "gz model -f " + temp_sdf + \
     " -m " + args.model + "_" + str(args.id) + \
-    " -x " + str(args.x) + \
-    " -y " + str(args.y) + \
-    " -z " + str(args.z+z_min) + \
-    " -Y " + str(args.Y)
+    " -x " + str(spawn_x) + \
+    " -y " + str(spawn_y) + \
+    " -z " + str(spawn_z + z_min) + \
+    " -Y " + str(spawn_yaw)
     subprocess.call(gzmodel_args, shell=True)
 
 if __name__ == "__main__":
